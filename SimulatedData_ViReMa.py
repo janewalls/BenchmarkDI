31dea6ea309e38548fd5be0aa59eec9f430ca1cf
janewalls <walls.jane@outlook.com>
2021-08-06 12:47:33 +0100

    Can be run from command line

--- SimulatedData_ViReMa.py
+++ SimulatedData_ViReMa.py
@@ -1,4 +1,4 @@
-import random
+import random, sys, getopt
 import Bio
 
 from Bio.Seq import Seq
@@ -7,51 +7,77 @@ from Bio.SeqRecord import SeqRecord
 
 # ViReMa simulation code 
 
-# Adjustable parameters 
-fastaFile = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/example.fasta"
-maxLength = 188
-totalReads = 50
-count = 0
+def main(argv):
 
-readOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimulatedViReMa.fasta"
-summaryOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimulatedViReMa.csv"
-
-# Initialise 
-reads = []
-
-summaryFile = open(summaryOutput, "w")
-
-n = int(maxLength/2)
+	# Initialise adjustable parameters 
+	fastaFile = ""
+	outputDir = ""
+	maxLength = 0
+	totalReads = 0
+	count = 0
 
 
-for refGenome in SeqIO.parse(fastaFile, "fasta"):
-	lenGenome = len(refGenome) - n # Get length of genome (# of nucleotides)
 
-	while count <= totalReads:
-		while True:
-			pos1 = random.randint(1,lenGenome)
-			pos2 = random.randint(1,lenGenome)
+	# Get options/paramaters 
+	try:
+		opts, args = getopt.getopt(argv,"h:i:o:m:t:",["ifile=","odir=","mlen=", "tread="])
+	except getopt.GetoptError:
+		print("script.py -i <inputfile> -o <outputdir> -m <maxreadlength> -t <totalreads>")
+		sys.exit(1)
+	for opt, arg in opts:
+		if opt == '-h':
+			print("script.py -i <inputfile> -o <outputdir> -m <maxreadlength> -t <totalreads>")
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			fastaFile = str(arg)
+		elif opt in ("-o", "--odir"):
+			outputDir = str(arg)
+		elif opt in ("-m", "--mlen"):
+			maxLength = int(arg)
+		elif opt in ("-t", "--tread"):
+			totalReads = int(arg)
 
-			if ((pos2 > pos1 + n) or (pos1 > pos2 + n)):
-				break
+	# Initialise variables necessary for files
+	reads = []
+	readOutput = outputDir + "/SimulatedViReMa.fasta"
+	summaryOutput = outputDir + "/SimulatedViReMa.csv"
 
-		seq1 = str(refGenome.seq[pos1:(pos1+n)])
-		seq2 = str(refGenome.seq[pos2:(pos2+n)])
+	summaryFile = open(summaryOutput, "w")
 
-		count += 1
-		dI = seq1 + seq2
+	n = int(maxLength/2)
+
+	# Create reads
+	for refGenome in SeqIO.parse(fastaFile, "fasta"):
+		lenGenome = len(refGenome) - n # Get length of genome (# of nucleotides)
+
+		while count <= totalReads:
+			while True:
+				pos1 = random.randint(1,lenGenome)
+				pos2 = random.randint(1,lenGenome)
+
+				if ((pos2 > pos1 + n) or (pos1 > pos2 + n)):
+					break
+
+			seq1 = str(refGenome.seq[pos1:(pos1+n)])
+			seq2 = str(refGenome.seq[pos2:(pos2+n)])
+
+			count += 1
+			dI = seq1 + seq2
 
 
-		rec = SeqRecord(
-    		Seq(dI,),
-    		id="test|test|gb|ABC123.4|ABC123_4",
-    		description="test DIPs",
-		)	
+			rec = SeqRecord(
+				Seq(dI,),
+				id="test|test|gb|ABC123.4|ABC123_4",
+				description="test DIPs",
+			)	
 
-		reads.append(rec)
+			reads.append(rec)
 
-		summaryFile.write(str(count) + "\t" + str(pos1) + "\t" + str(pos1+n) + "\t" + str(pos2) + "\t" + str(pos2+n) + "\n")
+			summaryFile.write(str(count) + "\t" + str(pos1) + "\t" + str(pos1+n) + "\t" + str(pos2) + "\t" + str(pos2+n) + "\n")
 
-	SeqIO.write(reads, readOutput, "fasta")
-	
-	summaryFile.close()
+		SeqIO.write(reads, readOutput, "fasta")
+		
+		summaryFile.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
