e40304813d35cd3d961dcdcf81aead576fe02f38
janewalls <walls.jane@outlook.com>
2021-08-06 14:36:01 +0100

    Make command line arguments

--- SimulatedData_DITector_3cb.py
+++ SimulatedData_DITector_3cb.py
@@ -1,4 +1,4 @@
-import random
+import random, sys, getopt
 import Bio
 
 from Bio.Seq import Seq
@@ -7,50 +7,77 @@ from Bio.SeqRecord import SeqRecord
 
 # DITector - 3' copy back simulation code 
 
-# Adjustable parameters 
-fastaFile = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/example.fasta"
-maxLength = 188
-totalReads = 50
-count = 0
+def main(argv):
 
-readOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimDITecINDEL.fasta"
-summaryOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimDITecINDEL.csv"
 
-#Initialising lists
-reads = []
+	# Initialise adjustable parameters 
+	fastaFile = ""
+	maxLength = 0
+	totalReads = 0
+	count = 0
 
-summaryFile = open(summaryOutput, "w")
+	# Get options/paramaters 
+	try:
+		opts, args = getopt.getopt(argv,"h:i:o:m:t:",["ifile=","odir=","mlen=", "tread="])
+	except getopt.GetoptError:
+		print("script.py -i <inputfile> -o <outputdir> -m <maxreadlength> -t <totalreads>")
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print("script.py -i <inputfile> -o <outputdir> -m <maxreadlength> -t <totalreads>")
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			fastaFile = str(arg)
+		elif opt in ("-o", "--odir"):
+			outputDir = str(arg)
+		elif opt in ("-m", "--mlen"):
+			maxLength = int(arg)
+		elif opt in ("-t", "--tread"):
+			totalReads = int(arg)
 
-for refGenome in SeqIO.parse(fastaFile, "fasta"):
-	lenGenome = len(refGenome) # Get length of genome (# of nucleotides)
 
-	while count <= totalReads:
-		while True:
-			frag1 = random.randint(1,maxLength)
-			frag2 = maxLength - frag1
-			bP = random.randint(frag1,(lenGenome - frag2))
-			rI = random.randint(frag1,(lenGenome - frag2))
+	#Initialising other
+	reads = []
+	readOutput = outputDir + "/SimulatedDITector3cb.fasta"
+	summaryOutput = outputDir + "/SimulatedDITector3cb.csv"
+
+	summaryFile = open(summaryOutput, "w")
+
+
+	# Create reads
+	for refGenome in SeqIO.parse(fastaFile, "fasta"):
+		lenGenome = len(refGenome) # Get length of genome (# of nucleotides)
+
+		while count <= totalReads:
+			while True:
+				frag1 = random.randint(1,maxLength)
+				frag2 = maxLength - frag1
+				bP = random.randint(frag1,(lenGenome - frag2))
+				rI = random.randint(frag1,(lenGenome - frag2))
+				
+				if bP < rI:
+					break
 			
-			if bP < rI:
-				break
-		
-		seq1 = str(refGenome.seq[(bP-frag1):bP]) 
-		seq2 = str(refGenome.seq[rI:(rI+frag2)]) # Finds sequence
-		seq3 = str(refGenome.seq[rI:(rI+frag2)].reverse_compliment()) # Then reverse compliment for 3'
+			seq1 = str(refGenome.seq[(bP-frag1):bP]) 
+			seq2 = str(refGenome.seq[rI:(rI+frag2)]) # Finds sequence
+			seq3 = str(refGenome.seq[rI:(rI+frag2)].reverse_compliment()) # Then reverse compliment for 3'
 
-		count += 1
-		dI = seq1 + seq2 + seq3
+			count += 1
+			dI = seq1 + seq2 + seq3
 
-		rec = SeqRecord(
-			Seq(dI,),
-			id="test|test|gb|ABC123.4|ABC123_4",
-			description="test DIPs",
-		)	
+			rec = SeqRecord(
+				Seq(dI,),
+				id="test|test|gb|ABC123.4|ABC123_4",
+				description="test DIPs",
+			)	
 
-		reads.append(rec)
+			reads.append(rec)
 
-		summaryFile.write(str(count) + "\t" + str(bP-frag1) + "\t" + str(bP) + "\t" + str(rI) + "\t" + str(rI+frag2) + "\n")
+			summaryFile.write(str(count) + "\t" + str(bP-frag1) + "\t" + str(bP) + "\t" + str(rI) + "\t" + str(rI+frag2) + "\n")
 
-	SeqIO.write(reads, readOutput, "fasta")
+		SeqIO.write(reads, readOutput, "fasta")
 
-	summaryFile.close()
+		summaryFile.close()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
--- SimulatedData_DITector_INDEL.py
+++ SimulatedData_DITector_INDEL.py
@@ -1,4 +1,4 @@
-import random
+import random, sys, getopt
 import Bio
 
 from Bio.Seq import Seq
@@ -7,49 +7,73 @@ from Bio.SeqRecord import SeqRecord
 
 # DITector - INDEL simulation code 
 
-# Adjustable parameters 
-fastaFile = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/example.fasta"
-maxLength = 188
-totalReads = 50
-count = 0
+def main(argv):
 
-readOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimDITecINDEL.fasta"
-summaryOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimDITecINDEL.csv"
+	# Initialise adjustable parameters 
+	fastaFile = ""
+	maxLength = 0
+	totalReads = 0
+	count = 0
 
-#Initialising lists
-reads = []
+# Get options/paramaters 
+	try:
+		opts, args = getopt.getopt(argv,"h:i:o:m:t:",["ifile=","odir=","mlen=", "tread="])
+	except getopt.GetoptError:
+		print("script.py -i <inputfile> -o <outputdir> -m <maxreadlength> -t <totalreads>")
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print("script.py -i <inputfile> -o <outputdir> -m <maxreadlength> -t <totalreads>")
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			fastaFile = str(arg)
+		elif opt in ("-o", "--odir"):
+			outputDir = str(arg)
+		elif opt in ("-m", "--mlen"):
+			maxLength = int(arg)
+		elif opt in ("-t", "--tread"):
+			totalReads = int(arg)
 
-summaryFile = open(summaryOutput, "w")
 
-for refGenome in SeqIO.parse(fastaFile, "fasta"):
-	lenGenome = len(refGenome) # Get length of genome (# of nucleotides)
+	#Initialising other
+	reads = []
+	readOutput = outputDir + "/SimulatedDITectorIndel.fasta"
+	summaryOutput = outputDir + "/SimulatedDITectorIndel.csv"
 
-	while count <= totalReads:
-		while True:
-			frag1 = random.randint(1,maxLength)
-			frag2 = maxLength - frag1
-			bP = random.randint(frag1,(lenGenome - frag2))
-			rI = random.randint(frag1,(lenGenome - frag2))
+	summaryFile = open(summaryOutput, "w")
+
+	for refGenome in SeqIO.parse(fastaFile, "fasta"):
+		lenGenome = len(refGenome) # Get length of genome (# of nucleotides)
+
+		while count <= totalReads:
+			while True:
+				frag1 = random.randint(1,maxLength)
+				frag2 = maxLength - frag1
+				bP = random.randint(frag1,(lenGenome - frag2))
+				rI = random.randint(frag1,(lenGenome - frag2))
+				
+				if bP < rI:
+					break
 			
-			if bP < rI:
-				break
-		
-		seq1 = str(refGenome.seq[(bP-frag1):bP])
-		seq2 = str(refGenome.seq[rI:(rI+frag2)])
+			seq1 = str(refGenome.seq[(bP-frag1):bP])
+			seq2 = str(refGenome.seq[rI:(rI+frag2)])
 
-		count += 1
-		dI = seq1 + seq2
+			count += 1
+			dI = seq1 + seq2
 
-		rec = SeqRecord(
-			Seq(dI,),
-			id="test|test|gb|ABC123.4|ABC123_4",
-			description="test DIPs",
-		)	
+			rec = SeqRecord(
+				Seq(dI,),
+				id="test|test|gb|ABC123.4|ABC123_4",
+				description="test DIPs",
+			)	
 
-		reads.append(rec)
+			reads.append(rec)
 
-		summaryFile.write(str(count) + "\t" + str(bP-frag1) + "\t" + str(bP) + "\t" + str(rI) + "\t" + str(rI+frag2) + "\n")
+			summaryFile.write(str(count) + "\t" + str(bP-frag1) + "\t" + str(bP) + "\t" + str(rI) + "\t" + str(rI+frag2) + "\n")
 
-	SeqIO.write(reads, readOutput, "fasta")
+		SeqIO.write(reads, readOutput, "fasta")
 
-	summaryFile.close()
+		summaryFile.close()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
