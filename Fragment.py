fd303c71932bc962c9b01a9ccc1196c2f5a8034e
janewalls <walls.jane@outlook.com>
2021-08-16 10:08:06 +0100

    Making command runnable

--- Fragment.py
+++ Fragment.py
@@ -1,34 +1,77 @@
-import random 
+import random, sys, argparse
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 
-fastaFile="/Users/janewalls/Documents/VS_CODE/MastersProjectDI/SimMultiSeg.fasta"
-
-fragOutput = "/Users/janewalls/Documents/VS_CODE/MastersProjectDI/fragmentTest.fasta"
-
-allReads = list(SeqIO.parse(fastaFile, "fasta"))
-
-n = 10 # number of fragments per read
+# Initialise variables
+fastaFile= ""
+fragOutput = ""
+n = 0 
+l = 0
+s = 0
 
 fragments = []
 
-for read in allReads:
-    for i in range(n):
-        l = random.randint(300,400)
-        if l > len(read): 
-            break
-        pos = random.randint(0,(len(read)-l)) #Finds start position for read
-        frag = str(read.seq[pos:(pos+l)]) # Gets sequence
+def main(argv):
 
-        rec = SeqRecord(
-			Seq(frag,),
-			id=str(read.id),
-			description=str(pos) + "-" + str(pos+l),
-		)	
+	parser = argparse.ArgumentParser()
 
-        fragments.append(rec)
+	# Flagged arguments
+	parser.add_argument('--file', '-f', required=True, help='Input fasta')
+	parser.add_argument('--out', '-o', required=True, help='Output fasta')
+	parser.add_argument('--num', '-n', required=True, help='Number of fragments per read')
+	parser.add_argument('--len', '-l', required=False, help='Average read length')
+	parser.add_argument('--std', '-s', required=True, help='Length standard deviation')
 
-SeqIO.write(fragments, fragOutput, "fasta") # Save fragments to fasta file 
+	args = parser.parse_args()
 
+	# Parsing arguments and assigning to variables
+	if args.file:
+		fastaFile = args.file
+	else:
+		print("Fasta file needed!")
+
+	if args.out:
+		fragOutput = args.outdir
+	else:
+		print("Output fasta")
+
+	if args.num:
+		n = args.num
+	else:
+		print("Number of fragments per read needed")
+	
+	if args.len:
+		l = args.len
+	else:
+		print("Average read length")
+
+	if args.std:
+		s = args.std
+	else:
+		print("Length standard deviation")
+
+
+	allReads = list(SeqIO.parse(fastaFile, "fasta"))
+
+	for read in allReads:
+		for i in range(n):
+			ln = random.randint(l-s,l+s)
+			if ln > len(read): 
+				break
+			pos = random.randint(0,(len(read)-ln)) #Finds start position for read
+			frag = str(read.seq[pos:(pos+ln)]) # Gets sequence
+
+			rec = SeqRecord(
+				Seq(frag,),
+				id=str(read.id),
+				description=str(pos) + "-" + str(pos+ln),
+			)	
+
+			fragments.append(rec)
+
+	SeqIO.write(fragments, fragOutput, "fasta") # Save fragments to fasta file 
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
 
